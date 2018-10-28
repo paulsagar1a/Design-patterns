ee4da2ef3aa1e9c815127b4fb8e1dbfd9c614068
paulsagar1a <paulsagar1a@gmail.com>
2018-10-28 22:05:02 +0530

    singleton design pattern package info

--- creational/singleton/package-info.java
+++ creational/singleton/package-info.java
@@ -1,4 +1,4 @@
-/*******************************************************
+/****************************************************************
  * @author SAGAR PAUL (paulsagar1a)
  * @category Design Patters
  * 
@@ -6,14 +6,18 @@
  * a class and ensures that only one instance of the class
  * exists in JVM
  * In this package we will discuss about below topics:
- * Eager initialization
- * Static block initialization
- * Lazy Initialization
- * Thread Safe Singleton
- * Double checked singleton
- * Bill Pugh Singleton Implementation
- * Using Reflection to destroy Singleton Pattern
- * Enum Singleton
- * Serialization and Singleton 
- *******************************************************/
+ * ---> Eager initialization
+ * ---> Static block initialization
+ * ---> Lazy Initialization
+ * ---> Thread Safe Singleton
+ * ---> Double checked singleton
+ * ---> Bill Pugh Singleton Implementation
+ * ---> Using Reflection to destroy Singleton Pattern
+ * ---> Enum Singleton
+ * ---> Serialization and Singleton 
+ * 
+ * Usages:
+ *  ---> java.lang.Runtime
+ *  ---> logger, configuration files, case, hardware interface access etc
+ *********************************************************************/
 package creational.singleton;
